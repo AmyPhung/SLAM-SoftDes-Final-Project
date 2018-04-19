d232e36491c7d365752d49622c40b7b89713e3c8
nathanestill <nathan.estill@students.olin.edu>
2018-04-19 16:54:26 -0400

    messing wtih the algorithm

--- Navigator.py
+++ Navigator.py
@@ -39,7 +39,7 @@ def Navigation(start,finish, map):
     solution = map # makes a solution map
     x_size = map.size[0] # the x length of the image
     y_size = map.size[1] # the y length of the image
-    initialNumber = 10000 # sets the initial values for the Gs, Fs and Hs
+    initialNumber = 1000 # sets the initial values for the Gs, Fs and Hs
     Gs = [[initialNumber for y in range(y_size)] for x in range(x_size)] # initializes a matrix of Gs for each pixel
     pastCoord = [start] # initializes the starting coordinate
     cost = 0 # initializes the current cost
@@ -49,7 +49,7 @@ def Navigation(start,finish, map):
             if(i[0]==finish[0] and i[1]==finish[1]): # if the coordinate is the finish, exit the loop. We're done!
                 Gs[i[0]][i[1]] = cost
                 break
-            if(i[0] > 0 and i[0] < x_size and i[1] > 0 and i[1] < y_size): # make sure the number is in bounds
+            try: # make sure the number is in bounds
                 if(Gs[i[0]][i[1]] == initialNumber): # check to see if the number has been dealt with before
                     available = True # initializing the availability to true
                     for j in range(sizeOfRobot): # run through the edges of the square around the robot
@@ -88,6 +88,8 @@ def Navigation(start,finish, map):
                         newCoord = newCoord + [(i[0]+1,i[1]),(i[0]-1,i[1]),(i[0],i[1]+1),(i[0],i[1]-1)] # adds the coordinates the surround the previous one to the list to check
                     else:
                         Gs[i[0]][i[1]] = 9999
+            except:
+                print("its ok")
         pastCoord = newCoord # adds the new coordinates to the past coordinates to check next
         cost+=1 # add the costs
     oldPixel = finish # starts with the old pixels at the finish
@@ -121,10 +123,12 @@ def actualAStar(start,finish,map):
     Gs = [[initialNumber for y in range(y_size)] for x in range(x_size)] # initializes a matrix of Gs for each pixel
     Hs = [[initialNumber for y in range(y_size)] for x in range(x_size)] # initializes a matrix of Gs for each pixel
     Fs = [[initialNumber for y in range(y_size)] for x in range(x_size)] # initializes a matrix of Gs for each pixel
+    Gs[start[0]][start[1]] = 0
     pastCoord = start # initializes the starting coordinate
     openList = {}
     closedList = []
     Goal = False
+    randomCounter = 0
     while(not Goal):
         adjacentCoords = [(pastCoord[0]+1,pastCoord[1]),(pastCoord[0]-1,pastCoord[1]),(pastCoord[0],pastCoord[1]+1),(pastCoord[0],pastCoord[1]-1)]
         for i in adjacentCoords:
@@ -171,11 +175,33 @@ def actualAStar(start,finish,map):
                         closedList.append(i)
         if(finish in openList):
             Goal = True
-        pastCoord = min(openList, key=openList.get)
-        print(pastCoord)
+        min_val = min(openList.itervalues())
+        goodKeys = [k for k, v in openList.iteritems() if v == min_val]
+        HDict = {}
+        for i in goodKeys:
+            HDict[i] = Hs[i[0]][i[1]]
+        pastCoord = min(HDict, key=HDict.get)
         solution.putpixel(pastCoord,(255,0,0))
         openList.pop(pastCoord)
         closedList.append(pastCoord)
+    oldPixel = finish # starts with the old pixels at the finish
+    ListOfCoordinates = [finish]
+    coords = [finish]
+    while(oldPixel in coords): # while we havn't gotten to the start yet
+        coords = [(oldPixel[0]+1,oldPixel[1]),(oldPixel[0]-1,oldPixel[1]),(oldPixel[0],oldPixel[1]+1),(oldPixel[0],oldPixel[1]-1)] # checks the 4 surounding pixels
+        Gvalues = [10000,10000,10000,10000] # initializes their values to 0
+        for i in range(len(coords)): # puts their values in
+            try:
+                Gvalues[i] = Gs[coords[i][0]][coords[i][1]]
+            except:
+                Gvalues[i] = 10000
+        if(sum(Gvalues) < 20): # if the sum of them is less than 10, the start has been reached
+            break
+        print(Gvalues)
+        coordToGoTo = coords[Gvalues.index(min(Gvalues))] # adds the coordinate that we want to go to next.
+        ListOfCoordinates = [coordToGoTo] + ListOfCoordinates
+        solution.putpixel(coordToGoTo,(0,255,0)) # makes that coordinate red
+        oldPixel = coordToGoTo # does it again
     solution.save("Maps/solution.png")
 def fromPathToVectors(ListOfCoordinates):
     """
@@ -208,5 +234,5 @@ def forward(pixels):
     print('forward')
 if __name__ == '__main__':
     start_time = time.time()
-    Navigation((300,300),(600,200),"Maps/MapNon-Fill.png")
+    actualAStar((100,475),(600,200),"Maps/MapNon-Fill.png")
     print("--- %s seconds ---" % (time.time() - start_time))
