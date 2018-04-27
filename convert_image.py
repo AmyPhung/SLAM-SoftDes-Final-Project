573011eb43782fc922acdeaa5ab7e37f3d3cea14
Sherrie Shen <xierui.shen@students.olin.edu>
2018-04-27 14:27:33 -0400

    add all world files

--- convert_image.py
+++ convert_image.py
@@ -1,17 +1,15 @@
 import cv2
 
 class Map:
-    def __init__(self,filename):
+    def __init__(self, filename):
         self.map_img = filename
 
-    def convert_pgm_to_png(self,name):
-        im = cv2.imread(self.map_img,0)
+    def convert_pgm_to_png(self, name):
+        im = cv2.imread(self.map_img, 0)
         color_im = cv2.cvtColor(im, cv2.COLOR_GRAY2BGR)
-        cv2.imwrite('./Maps/%s' %name,color_im)
-
-
+        blur_im = cv2.GaussianBlur(color_im, (5, 5), 0)
+        cv2.imwrite('./Maps/%s' % name, blur_im)
 
 if __name__ == '__main__':
     lib = Map('./Maps/library_lower_day2.pgm')
-    lib.convert_pgm_to_png('map2.png')
-
+    lib.convert_pgm_to_png('library_lower_day2.png')
